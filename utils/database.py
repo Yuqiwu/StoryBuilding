4deedc71f526fa90a98521c8d72736630953b18e
kchen36 <kchen36@stuy.edu>
2017-10-25 22:01:26 -0400

    need to test getStory

--- utils/database.py
+++ utils/database.py
@@ -4,7 +4,7 @@ f = "../storybase.db"
 db = sqlite3.connect(f)
 c = db.cursor()
 
-c.execute('CREATE TABLE IF NOT EXISTS stories (name TEXT, content TEXT);')
+c.execute('CREATE TABLE IF NOT EXISTS stories (title TEXT, content TEXT);')
 
 def addUser(username, password):
     if getPass(username) == None:
@@ -21,13 +21,27 @@ def getPass(user):
         return None
     else:
         return result[0][0]
-    
-    
         
-def newStory(title, line, content, user):
+def newStory(title, content, user):
     c.execute('INSERT INTO stories values(?, ?)', (title, content) )
-    c.execute( 'CREATE TABLE "%s" (line INTEGER PRIMARY KEY,content TEXT, author TEXT);' %(title) )
+    c.execute( 'CREATE TABLE "%s" (content TEXT, author TEXT);' %(title) )
+    addToStory(title, content, user)
+    
+def addToStory(title, content, user):
     c.execute('INSERT INTO "%s" VALUES( "%s", "%s");' %(title,content,user) )
     db.commit()
-              
-newStory('the cult of the oreoss','oreos are delicious','bob')
+
+def getStory(story,name):
+    c.execute('SELECT author FROM "%s" WHERE username=\'%s\';' %(story,name) )
+    result = c.fetchall()
+    if result == []:
+        c.execute('SELECT content FROM stories WHERE title = "%s";' %(story) )
+        result = c.fetchall()
+        return result[0][0]
+    else:
+        c.execute('SELECT content FROM "%s"' %(story) )
+        result = c.fetchall()
+        s = ""
+        for content in result:
+            s = s + content[0]
+        return s
