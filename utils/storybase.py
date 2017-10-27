aba0c999e43de8cc5ecff6b55afbcbc00115112c
Kyle Lin <klin12@stuy.edu>
2017-10-27 08:17:25 -0400

    Fixed formatting and makde variable and parameter names consistent

--- utils/storybase.py
+++ utils/storybase.py
@@ -4,26 +4,33 @@ f = "../storybase.db"
 db = sqlite3.connect(f)
 c = db.cursor()
 
+# Ensures the stories database exists
 c.execute('CREATE TABLE IF NOT EXISTS stories (title TEXT);')
+# Ensures the user database exists
+c.execute('CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT);')
 
+# Creates a new table containing the first line of the new story
 def newStory(title, content, user):
-    c.execute('INSERT INTO stories values(?);', (title) )
-    c.execute( 'CREATE TABLE "%s" (show INTEGER,content TEXT, author TEXT);' %(title) )
+    c.execute('INSERT INTO stories values(?);', (title))
+    c.execute('CREATE TABLE "%s" (content TEXT, user TEXT);' %(title))
     addToStory(title, content, user)
     
+# Adds a line to the existing story
 def addToStory(title, content, user):
-    c.execute('INSERT INTO "%s" VALUES("%s", "%s");' %(title,content,user) )
+    c.execute('INSERT INTO "%s" VALUES("%s", "%s");' %(title,content,user))
     db.commit()
 
-def getStory(story,name):
-    c.execute('SELECT author FROM "%s" WHERE author=\'%s\';' %(story,name) )
+# Returns the latest line of the story if the user has not added
+# Returns the entire story if the user has added
+def getStory(title, user):
+    c.execute('SELECT * FROM "%s" WHERE user=\'%s\';' %(title, user))
     result = c.fetchall()
     if result == []:
-        c.execute('SELECT content FROM "%s"' %(story) )
+        c.execute('SELECT content FROM "%s"' %(title))
         result = c.fetchall()
         return result[-1][0]
     else:
-        c.execute('SELECT content FROM "%s"' %(story) )
+        c.execute('SELECT content FROM "%s"' %(title))
         result = c.fetchall()
         s = ""
         for content in result:
--- utils/userbase.py
+++ utils/userbase.py
@@ -3,26 +3,28 @@ import sqlite3
 f = "../storybase.db"
 db = sqlite3.connect(f)
 c = db.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT);')
 
+# Adds a username/password combination to the user table
 def addUser(username, password):
     if getPass(username) == None:
-        c.execute('INSERT INTO users VALUES(\'%s\', \'%s\');' %(username, password) )
+        c.execute('INSERT INTO users VALUES(\'%s\', \'%s\');' %(username, password))
         db.commit()
         return True
     else:
         return False
 
-def getPass(user):
-    c.execute('SELECT password FROM users WHERE username=\'%s\';' %(user) )
+# Get the password of a user
+def getPass(username):
+    c.execute('SELECT password FROM users WHERE username=\'%s\';' %(username))
     result = c.fetchall()
     if result == []:
         return None
     else:
         return result[0][0]
 
+# Login function
 #note: cookies will be created and deleted in the file that does the flask stuff
-def login(username,password): #returns 1 if successful, 0 if not successful, and -1 if account does not exist
+def login(username, password): #returns 1 if successful, 0 if not successful, and -1 if account does not exist
     p = getPass(username)
     if(p == None):
         return -1
@@ -32,19 +34,21 @@ def login(username,password): #returns 1 if successful, 0 if not successful, and
     else:
         return 0
 
-def makeLoginCookie(username,password): #call this function after login is successful
+# Stores the login in a cookie
+def makeLoginCookie(username, password): #call this function after login is successful
     session['username'] = username
 	session['password'] = password
 	
-
+# Deletes the cookie that stores user login
 def deleteLoginCookie(): #call this function if logging out or logging in with cookie is unsuccessful
     if('username' in session):
 		session.pop('username')
 	if('password') in session):
 		session.pop('password')
 
+# Checks if a user login cookie exists
 def hasCookie():
-	if('user' in session and 'password' in session):
+	if('username' in session and 'password' in session):
 		return True
 	else:
 		return False
