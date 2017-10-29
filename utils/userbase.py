b60a537c5cd0bc476ee4c7a90ef6923598303926
Kyle Lin <klin12@stuy.edu>
2017-10-29 11:43:31 -0400

    snakecased all functions

--- utils/userbase.py
+++ utils/userbase.py
@@ -6,8 +6,8 @@ db = sqlite3.connect(f)
 c = db.cursor()
 
 # Adds a username/password combination to the user table
-def addUser(username, password):
-    if getPass(username) == None:
+def add_user(username, password):
+    if get_pass(username) == None:
         c.execute('INSERT INTO users VALUES(\'%s\', \'%s\');' %(username, password))
         db.commit()
         return True
@@ -15,7 +15,7 @@ def addUser(username, password):
         return False
 
 # Get the password of a user
-def getPass(username):
+def get_pass(username):
     c.execute('SELECT password FROM users WHERE username=\'%s\';' %(username))
     result = c.fetchall()
     if result == []:
@@ -26,33 +26,33 @@ def getPass(username):
 # Login function
 #note: cookies will be created and deleted in the file that does the flask stuff
 def login(username, password): #returns 1 if successful, 0 if not successful, and -1 if account does not exist
-    p = getPass(username)
+    p = get_pass(username)
     if(p == None):
         return -1
     elif(p == password):
-        makeCookie(username,password)
+        make_login_cookie(username,password)
         return 1
     else:
         return 0
 
 #checks to see if user is in a session
-def inSession():
-	return (hasCookie() and session.get('password') == getPass(session.get('username'))
+def in_session():
+	return (has_cookie() and session.get('password') == get_pass(session.get('username'))
 
 # Stores the login in a cookie
-def makeLoginCookie(username, password): #call this function after login is successful
+def make_login_cookie(username, password): #call this function after login is successful
     session['username'] = username
     session['password'] = password
 
 # Deletes the cookie that stores user login
-def deleteLoginCookie(): #call this function if logging out or logging in with cookie is unsuccessful
+def delete_login_cookie(): #call this function if logging out or logging in with cookie is unsuccessful
     if('username' in session):
 		session.pop('username')
     if('password' in session):
 		session.pop('password')
 
 # Checks if a user login cookie exists
-def hasCookie():
+def has_cookie():
 	if('username' in session and 'password' in session):
 		return True
 	else:
